286026542514dafe2fd999a8a8dcf37658fce9f3
Myleshen <mylesuge@gmail.com>
2017-07-27 14:52:01 +0530

    Partially Success

---
+++ Src/Day4_Assignment 3.py
@@ -0,0 +1,20 @@
+def alive_person(size,step):
+    persons = []
+    for index_loop in range(1, size + 1):
+        persons.append(index_loop)
+    index = 0
+    counter = 0
+    while (len(persons) > 1):
+        counter = counter + 1
+        if index > len(persons) - 1 :
+            index = 0
+        if counter % step == 0:
+            persons.pop(index)
+        index = index + 1
+    return persons
+
+size = int(input())
+step = int(input())
+print(alive_person(size,step))
+
+
